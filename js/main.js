6f720b7d35ed80bf164c2d5e2d1e86118f05df48
Maria Tsvetkova <tsvetkovamariia@gmail.com>
2017-03-25 17:47:51 +0600

    main slider

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ $(document).ready(function(){
 	toggle.click(function(e){
 		menu.slideToggle();
 	});
+
+	$('.slider').slick({
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>'
+	});
 });
