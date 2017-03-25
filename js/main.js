43feaf7c47465febdab528c81d7c556740043508
Maria Tsvetkova <tsvetkovamariia@gmail.com>
2017-03-25 20:48:10 +0600

    add smooth scroll

--- js/main.js
+++ js/main.js
@@ -10,4 +10,8 @@ $(document).ready(function(){
 		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
 		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>'
 	});
+
+
 });
+
+smoothScroll.init();
